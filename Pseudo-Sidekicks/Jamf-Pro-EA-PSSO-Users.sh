5e004ee2d4872d5d4ae8a2630f3b85e93841c738
Kevin <86124561+Macjutsu@users.noreply.github.com>
2026-06-12 17:12:38 -0400

    1.0.0.0-beta6

--- Pseudo-Sidekicks/Jamf-Pro-EA-PSSO-Users.sh
+++ Pseudo-Sidekicks/Jamf-Pro-EA-PSSO-Users.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-# This Jamf Pro Extension Attribute returns the list of Platform SSO registered users.
+# This Jamf Pro Extension Attribute returns the list of Platform SSO enabled users.
+# Expected output includes both the local user account name and the redacted IDp account name (due to macOS limitations) or accounts that need to repair their registration.
 # shellcheck disable=SC2012,SC2024,SC2207
 
-local_users=($(dscl . list /Users | grep -v '^_'))
-psso_enabled_users=()
-
-for user_name in "${local_users[@]}";do
-	[[ "$(dscl . read /Users/"${user_name}" dsAttrTypeStandard:AltSecurityIdentities 2>/dev/null | awk -F'SSO:' '/PlatformSSO/ {print $2}')" ]] && psso_enabled_users+=("${user_name}")
+user_accounts=($(dscl . list /Users | grep -v '^_'))
+psso_user_accounts=()
+for user_account in "${user_accounts[@]}";do
+	[[ "$(dscl . read /Users/"${user_account}" dsAttrTypeStandard:AltSecurityIdentities 2>/dev/null | awk -F'SSO:' '/PlatformSSO/ {print $2}')" ]] && psso_user_accounts+=("${user_account}")
 done
-
 IFS=$'\n'
 
-if [[ ${#psso_enabled_users[@]} -gt 0 ]]; then
-	echo "<result>${psso_enabled_users[*]}</result>"
+if [[ ${#psso_user_accounts[@]} -gt 0 ]]; then
+	psso_verified_user_accounts=()
+	for psso_user_account in "${psso_user_accounts[@]}"; do
+		psso_user_login_name=$(launchctl asuser "$(id -u "${psso_user_account}" 2> /dev/null)" sudo -u "${psso_user_account}" app-sso platform -s 2> /dev/null | sed -e '1,/User Configuration:/d' | tr -d '\\' | jq -r '.userLoginConfiguration.loginUserName' 2> /dev/null)
+		[[ -n "${psso_user_login_name}" ]] && psso_verified_user_accounts+=($(echo "${psso_user_account}:${psso_user_login_name}"))
+		[[ -z "${psso_user_login_name}" ]] && psso_verified_user_accounts+=($(echo "${psso_user_account}:Needs Platform SSO Repair"))
+	done
+fi
+
+if [[ ${#psso_verified_user_accounts[@]} -gt 0 ]]; then
+	echo "<result>${psso_verified_user_accounts[*]}</result>"
 else
-	echo "<result>No Platform SSO Users</result>"
+	echo "<result>No Platform SSO Registered Users</result>"
 fi
 
 exit 0
